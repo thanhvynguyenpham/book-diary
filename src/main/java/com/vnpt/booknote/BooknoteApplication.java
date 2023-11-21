3d473f7c8b29dbe304fedb48190fae1f685e48c0
thanhvynguyenpham <thanhvy.forwork@gmail.com>
2023-11-21 21:53:58 +0700

    init spring project

---
+++ src/main/java/com/vnpt/booknote/BooknoteApplication.java
@@ -0,0 +1,13 @@
+package com.vnpt.booknote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooknoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooknoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnpt/booknote/BooknoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnpt.booknote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooknoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
